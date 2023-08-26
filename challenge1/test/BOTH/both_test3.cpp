64fee2c8331526c80ba8036520ec9ec5a359e38b
John Leidel <john.leidel@gmail.com>
2023-08-25 21:00:48 -0500

    adding tests for both patterns

---
+++ challenge1/test/BOTH/both_test3.cpp
@@ -0,0 +1,23 @@
+// RUN: clang++ -cc1 -load %shlibdir/libChallenge1Lib%shlibext -plugin Challenge1 \
+// RUN: -plugin-arg-Challenge1 -enable-msg1 -plugin-arg-Challenge1 -enable-msg2 %s 2>&1
+//
+// Copyright (C) 2017-2023 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+
+int recurse(int A){
+  if( A > 0 ){
+    return A + recurse(A-1);
+  }else{
+    return A;
+  }
+}
+
+int main(int argc, char **argv){
+  return recurse(argc);
+}
+
+// EOF
---
+++ challenge1/test/BOTH/both_test6.cpp
@@ -0,0 +1,21 @@
+// RUN: clang++ -cc1 -load %shlibdir/libChallenge1Lib%shlibext -plugin Challenge1 \
+// RUN: -plugin-arg-Challenge1 -enable-msg1 -plugin-arg-Challenge1 -enable-msg2 %s 2>&1
+//
+// Copyright (C) 2017-2023 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+
+
+void functionCall(){
+  unsigned VAR1;
+}
+
+int main(int argc, char **argv){
+  void functionCall();
+  return 0;
+}
+
+// EOF
